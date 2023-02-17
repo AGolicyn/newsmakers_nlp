c7af55cc21d3b4525f827e41dafc07bffc124195
Artem Golicyn <win.rus44@gmail.com>
2023-02-17 04:03:38 +0300

    forget about wordclud

---
+++ src/processing/word_cloud.py
@@ -0,0 +1,36 @@
+import datetime
+import time
+import pathlib
+
+import numpy as np
+from wordcloud import WordCloud
+from loguru import logger
+
+
+def build_wordcloud(entity: str,
+                    frequencies: dict,
+                    date: datetime.date,
+                    lang: str,
+                    country: str):
+    logger.debug(f'Building wordcloud for {country} {entity}')
+
+    if not frequencies:
+        logger.info(f'Empty frequencies for {entity}')
+        return
+    x, y = np.ogrid[:1000, :1000]
+    mask = (x - 500) ** 2 + (y - 500) ** 2 > 420 ** 2
+    mask = 255 * mask.astype(int)
+
+    wordcloud = WordCloud(
+        background_color='white',
+        colormap='Set2',
+        collocations=False,
+        repeat=True,
+        mask=mask,
+        prefer_horizontal=0.9,
+    )
+    wordcloud.generate_from_frequencies(frequencies=frequencies)
+    p = pathlib.Path(f'./images/{date}/{country}/')
+    p.mkdir(parents=True, exist_ok=True)
+    wordcloud.to_file(f'images/{date}/{country}/{entity}.png')
+    logger.debug(f'Stored to {p}')

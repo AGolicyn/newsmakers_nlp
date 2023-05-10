1178b1773bbf90fe3c68003731d2b6016d5c3295
Artem Golicyn <win.rus44@gmail.com>
2023-05-10 20:29:01 +0300

    Added UK to settings

--- src/nlp/processing/settings.py
+++ src/nlp/processing/settings.py
@@ -3,7 +3,7 @@ from enum import Enum
 from collections import namedtuple
 
 
-SUPPORTED_COUNTRIES = ["Russia", "USA", "Germany"]
+SUPPORTED_COUNTRIES = ["Russia", "USA", "Germany", "UK"]
 
 
 class Entity(Enum):
@@ -60,56 +60,17 @@ def language_manager(country: str) -> namedtuple:
                 "morphologizer",
             ],
         )
+    elif country == "UK":
+        return Lang(
+            country="UK",
+            lang="EN",
+            nlp=spacy.load("en_core_web_md"),
+            entities={
+                "GPE": Entity.Location.value,
+                "PERSON": Entity.Person.value,
+                "ORG": Entity.Organization.value,
+            },
+            pipes=["ner", "lemmatizer", "tagger", "attribute_ruler"],
+        )
     else:
         raise f"{country} NOT IMPLEMENTED"
-
-
-#
-# def _load_nlp(lang: str):
-#     if lang == 'RU':
-#         with open('/src/nlp/processing/nlp_data/ru_core_news_md.pickle', 'rb') as f:
-#             ru_nlp = pickle.load(f)
-#         return ru_nlp
-#     elif lang == 'EN':
-#         with open('/src/nlp/processing/nlp_data/en_core_web_md.pickle', 'rb') as f:
-#             en_nlp = pickle.load(f)
-#         return en_nlp
-#     elif lang == 'DE':
-#         with open('/src/nlp/processing/nlp_data/de_core_news_md.pickle', 'rb') as f:
-#             de_nlp = pickle.load(f)
-#         return de_nlp
-#     else:
-#         raise f'NLP for {lang} NOT INSTALLED'
-
-# print(Entity.Location.value)
-# nlp_2 = spacy.load("en_core_web_lg")
-# nlp_3 = spacy.load("de_core_news_lg")
-# nlp_1 = spacy.load("de_core_news_lg")
-# print(nlp_1)
-# nlp_1.to_disk('ru_core_news_lg')
-# time.sleep(2)
-
-# ru_nlp = Language()
-# ru_nlp.from_disk(path='./ru_core_news_lg')
-# print(my_nlp)
-
-# with open('nlp_data/de_core_news_md.pickle', 'wb') as f:
-#     pickle.dump(nlp_1, f)
-#
-# with open('nlp_data/en_core_web_lg.pickle', 'wb') as f:
-#     pickle.dump(nlp_2, f)
-#
-# with open('nlp_data/de_core_news_lg.pickle', 'wb') as f:
-#     pickle.dump(nlp_1, f)
-
-# with open('nlp_data/ru_core_news_md.pickle', 'rb') as f:
-#     ru = pickle.load(f)
-# with open('nlp_data/en_core_web_lg.pickle', 'rb') as f:
-#     en = pickle.load(f)
-# with open('nlp_data/de_core_news_lg.pickle', 'rb') as f:
-#     de = pickle.load(f)
-
-
-# print(nlp_1, ru)
-# print(nlp_2, en)
-# print(nlp_3, de)
